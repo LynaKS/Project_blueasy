f5dd746525ef8a49b365683d9d19195121fc3165
VasylynaS <46923273+VasylynaS@users.noreply.github.com>
2020-04-22 16:12:43 +0300

    Add files via upload

---
+++ Project_blueasy/main.js
@@ -0,0 +1,19 @@
+// let headerMenu = document.querySelector('.header_container');
+
+
+
+
+// window.addEventListener('scroll', () => {
+//   // const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+//   const scrolled = window.scrollX;
+
+//   if (Math.ceil(scrolled) - 3) {
+//     headerOne.style.cssText = `color: red;
+//     background-color: yellow;
+//     height: 100px;
+
+//   `;
+//   } else {
+//     alert('нічого не відбулось');
+//   }
+// });
